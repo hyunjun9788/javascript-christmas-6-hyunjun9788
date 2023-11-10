0ffe847e219d777f325236fdab4be96c52c173bf
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-10 14:19:06 +0900

    Feat: 방문 날짜 입력 함수 호출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console } from "@woowacourse/mission-utils";
+import InputView from "./InputView.js";
+import validation from "./Validation.js";
 class App {
-  async run() {}
+  constructor() {}
+  async run() {
+    await this.inputVisitDate();
+  }
+
+  async inputVisitDate() {
+    try {
+      const visitDate = await InputView.inputVisitDate();
+      this.visitDateNum = this.convertToNum(visitDate);
+      validation.isValidInputDate(this.visitDateNum);
+    } catch (e) {
+      Console.print(e.message);
+      await this.inputVisitDate();
+    }
+  }
+
+  convertToNum(Date) {
+    return Number(Date);
+  }
 }
 
 export default App;
