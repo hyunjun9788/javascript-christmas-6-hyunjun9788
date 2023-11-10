b6a3c9f458892c08131b8f547eabdfd55f8d1b61
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-10 21:15:52 +0900

    Fix: 클래스 변환 및 메뉴 포함 여부 확인 함수 구현

--- src/Validation.js
+++ src/Validation.js
@@ -1,12 +1,17 @@
-const Validation = {
+import MENU_ITEMS from "./constant/constants.js";
+class Validation {
+  constructor() {}
   isValidInputDate(date) {
-    if (date < 1 || date > 31) {
+    if (date < 1 || date > 31 || isNaN(date)) {
       throw new Error("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
-    if (isNaN(date)) {
-      throw new Error("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+  }
+
+  isValidInputMenuAndCount(isIncludeMenu) {
+    if (!isIncludeMenu) {
+      throw new Error("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
-  },
-};
+  }
+}
 
 export default Validation;
