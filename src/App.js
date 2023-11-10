29a1b441e926ef01e0d499e7878e59dbda928ba7
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-10 21:12:20 +0900

    Feat: 주문메뉴 입력 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import { Console } from "@woowacourse/mission-utils";
 import InputView from "./InputView.js";
-import validation from "./Validation.js";
+import Validation from "./Validation.js";
+import Common from "./Common.js";
 class App {
-  constructor() {}
+  constructor() {
+    this.validation = new Validation();
+    this.common = new Common();
+  }
   async run() {
     await this.inputVisitDate();
+    this.inputOrderInfo = await this.inputOrderMenuAndCount();
   }
 
   async inputVisitDate() {
     try {
       const visitDate = await InputView.inputVisitDate();
       this.visitDateNum = this.convertToNum(visitDate);
-      validation.isValidInputDate(this.visitDateNum);
+      this.validation.isValidInputDate(this.visitDateNum);
     } catch (e) {
       Console.print(e.message);
       await this.inputVisitDate();
+    }
+  }
+
+  async inputOrderMenuAndCount() {
+    try {
+      const orderMenu = await InputView.inputMenuAndCount();
+      const orderList = this.common.processOrderInfo(orderMenu);
+      const isIncludedMenu = this.common.isIncludeMenu(orderList);
+      this.validation.isValidInputMenuAndCount(isIncludedMenu);
+    } catch (e) {
+      Console.print(e.message);
+      await this.inputOrderMenuAndCount();
     }
   }
 
