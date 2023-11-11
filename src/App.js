778988e6ed2c8139cc25ee3e74c40b42bca527e4
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-11 13:20:58 +0900

    Feat: 전체 구매액 구하는 함수 및 미리보기 안내 문구 출력 함수 구현

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import { Console } from "@woowacourse/mission-utils";
 import InputView from "./InputView.js";
 import Validation from "./Validation.js";
 import Common from "./Common.js";
+import { MENU_ITEMS } from "./constant/constants.js";
+import OutputView from "./OutputView.js";
 class App {
   constructor() {
     this.validation = new Validation();
     this.common = new Common();
   }
   async run() {
-    await this.inputVisitDate();
+    this.visitDate = await this.inputVisitDate();
     await this.inputOrderMenuAndCount();
+    this.totalPurchasePrice = this.getTotalOrderPrice(this.orderList);
+    console.log(this.totalPurchasePrice);
+    this.printPreviewMessage(this.visitDateNum);
+    OutputView.printMenu();
   }
 
   async inputVisitDate() {
@@ -43,6 +49,27 @@ class App {
 
   sumInputCounts(orderList) {
     return orderList.reduce((total, order) => total + order.parsedCount, 0);
+  }
+
+  getTotalOrderPrice(orderList) {
+    let totalPrice = 0;
+
+    orderList.forEach((order) => {
+      const menuItem = order.menuItem;
+      const parsedCount = order.parsedCount;
+
+      for (const category in MENU_ITEMS) {
+        if (MENU_ITEMS[category][menuItem] !== undefined) {
+          const menuPrice = MENU_ITEMS[category][menuItem];
+          totalPrice += menuPrice * parsedCount;
+          break;
+        }
+      }
+    });
+    return totalPrice;
+  }
+  printPreviewMessage(date) {
+    Console.print(`12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리보기`);
   }
 }
 
