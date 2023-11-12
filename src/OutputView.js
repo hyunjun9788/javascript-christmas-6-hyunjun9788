3ea7dc96435dc8cac092411cc576ac380518e0dc
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-12 18:58:51 +0900

    Feat: 증정 메뉴 출력 함수 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -12,6 +12,10 @@ const OutputView = {
   printOriginalPurchasePrice(orginalTotalPrice) {
     Console.print(`<할인 전 총주문 금액>\n${orginalTotalPrice}원`);
   },
+  printGiftMenu(menu) {
+    Console.print("<증정 메뉴>");
+    Console.print(menu);
+  },
 };
 
 export default OutputView;
