18f35d6164d4c59d952613549584887ddbe85369
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-15 20:45:57 +0900

    Refactor: 중복 유효성 검사 함수 리팩토링

--- src/Validation.js
+++ src/Validation.js
@@ -1,7 +1,6 @@
 import { ERROR_MESSAGE, LIMIT_ORDER_COUNT } from "./constant/constants.js";
 
 class Validation {
-  constructor() {}
   isValidInputDate(date) {
     if (date < 1 || date > 31 || !Number.isInteger(date)) {
       throw new Error(ERROR_MESSAGE.INVALID_DATE);
@@ -13,21 +12,25 @@ class Validation {
       throw new Error(ERROR_MESSAGE.INVALID_ORDER);
     }
   }
-  isDuplicateMenu(menu, menuList) {
-    if (menuList.includes(menu)) {
+
+  isDuplicateMenu(uniqueMenuItems, orderList) {
+    if (uniqueMenuItems.size !== orderList.length) {
       throw new Error(ERROR_MESSAGE.INVALID_ORDER);
     }
   }
+
   isValidCount(count) {
     if (!Number.isInteger(count) || Number(count) === 0) {
       throw new Error(ERROR_MESSAGE.INVALID_ORDER);
     }
   }
+
   isLimitCount(sumCounts) {
     if (sumCounts > LIMIT_ORDER_COUNT) {
       throw new Error(ERROR_MESSAGE.LIMIT_SUM_COUNT);
     }
   }
+
   isBeverageOnlyOrder(isBeverageOnly) {
     if (isBeverageOnly) {
       throw new Error(ERROR_MESSAGE.BEVERAGE_ONLY_ORDER);
