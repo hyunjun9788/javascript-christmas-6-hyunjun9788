a7c3e3e9a0a2161acc2bd6c2e955213d969a0338
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-15 21:03:24 +0900

    Chore: 주석 제거

--- src/Badge.js
+++ src/Badge.js
@@ -3,6 +3,7 @@ import {
   STAR_BENEFIT_PRICE,
   TREE_BENEFIT_PRICE,
 } from "./constant/constants.js";
+
 class Badge {
   #name;
 
@@ -13,17 +14,12 @@ class Badge {
   getName() {
     return this.#name;
   }
+
   setName(name) {
-    // if (typeof name !== "string") throw "문자열 주셈";
-
-    // if (name.length === 0) throw "님 빈문자열 넣음";
-
-    // if (name !== "산타" && name !== "별" && name !== "현준") throw "유효하지 않은 badge값이 들어갔음";
-
     this.#name = name;
   }
 
-  badgeEvent(totalBenefitPrice) {
+  getBadgeName(totalBenefitPrice) {
     if (
       totalBenefitPrice >= STAR_BENEFIT_PRICE.MIN_PRICE &&
       totalBenefitPrice < STAR_BENEFIT_PRICE.MAX_PRICE
